3ab47e899bf54e8333586478f01bba32f02fbead
Daniel Vorhaug <dvorhaug@gmail.com>
2022-02-07 17:34:20 +0100

    Some fixes in analyze

--- raspi_analyze.py
+++ raspi_analyze.py
@@ -21,7 +21,7 @@ def raspi_import(path, channels=5):
 # Import data from bin file
 sample_period, data = raspi_import('adcData.bin')
 
-#data = signal.detrend(data, axis=0)  # removes DC component for each channel
+data = signal.detrend(data, axis=0)  # removes DC component for each channel
 sample_period *= 1e-6  # change unit to micro seconds
 
 # Generate time axis
@@ -32,24 +32,28 @@ t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
 freq = np.fft.fftfreq(n=num_of_samples, d=sample_period)
 spectrum = np.fft.fft(data, axis=0)  # takes FFT of all channels
 
+#Fixes frequency axis from -16kHz to 16kHz
+freq = np.fft.fftshift(freq)
+spectrum = np.fft.fftshift(spectrum)
+
 
 # Plot the results in two subplots
 # NOTICE: This lazily plots the entire matrixes. All the channels will be put into the same plots.
 # If you want a single channel, use data[:,n] to get channel n
 
-# for i in range(1,6):
-#     plt.subplot(5, 1, i)
-#     #plt.title("Time domain signal")
-#     plt.xlabel("Time [us]")
-#     plt.ylabel("Voltage")
-#     plt.plot(t, data[:,i-1])
+for i in range(1,6):
+    plt.subplot(5, 1, i)
+    #plt.title("Time domain signal")
+    plt.xlabel("Time [us]")
+    plt.ylabel("Voltage")
+    plt.plot(t, data[:,i-1])
 
-for i in range(3,6):
-    plt.subplot(3, 1, i-2)
-    #plt.title("Power spectrum of signal")
-    plt.xlabel("Frequency [Hz]")
-    plt.ylabel("Power [dB]")
-    plt.plot(freq[: math.ceil(len(freq)/2)], 20*np.log10(np.abs(spectrum[:math.ceil(len(freq)/2),i-1])))#, ".") # get the power spectrum
+#for i in range(3,6):
+#    plt.subplot(3, 1, i-2)
+#    #plt.title("Power spectrum of signal")
+#    plt.xlabel("Frequency [Hz]")
+#    plt.ylabel("Power [dB]")
+#    plt.plot(freq, 20*np.log10(np.abs(spectrum[:,i-1])))#, ".") # get the power spectrum
 
 # print(freq[: math.ceil(len(freq)/2)])
 # plt.subplot(2, 1, 1)
