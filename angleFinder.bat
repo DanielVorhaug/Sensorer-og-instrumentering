f54e2d524fe73c96bb6a1e30f1c5efb0c6601ef8
Carl Thyness <carl.thyness@gmail.com>
2022-02-21 10:30:22 +0100

    Made angleFinder.bat not update what part of the process it is doing
    Makes for a cleaner terminal, but in case of errors this change should be reverted

--- angleFinder.bat
+++ angleFinder.bat
@@ -1,3 +1,4 @@
-ssh pi@hostname.local "sudo /home/pi/adc_sampler/adc_sampler 31250"
-scp pi@hostname.local:/home/pi/adcData.bin adcData.bin
-py .\acoustics.py
+::This script does not update you on where in the process it currently is, nice for my use but may be trash in other settings
+@ssh pi@hostname.local "sudo /home/pi/adc_sampler/adc_sampler 31250"
+@scp pi@hostname.local:/home/pi/adcData.bin adcData.bin
+@py .\acoustics.py
