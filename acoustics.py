bc5bf173694f80d8f99fc94a6b28164f50ef6b68
Carl Thyness <carl.thyness@gmail.com>
2022-02-22 07:59:22 +0100

    Moved functionality from angleFinder.py into acoustics.py
    Allows for retention of result for later use

--- acoustics.py
+++ acoustics.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 import math
+from subprocess import call
+from time import sleep
 
 def make_correlations(data, wanted_axis1, wanted_axis2, wanted_axis3):
     # Makes three full crosscorrelations
@@ -19,19 +21,20 @@ def make_correlations(data, wanted_axis1, wanted_axis2, wanted_axis3):
 
 def get_angle(l12, l23, l13):
     angl = np.arctan(np.sqrt(3))*(-l12 - l13)/(-l12 +l13 + 2*l23)
+    #angl -= 1.05 #To control what is 0 radians
     if(l12 - l13 - 2*l23 < 0):
         angl = angl + np.pi
     
     return(angl)
 
-def find_lags(n12, n23, n13):
-    maxLags = 6
+def find_lags(n12, n23, n13, N_interp):
+    maxLags = 10*N_interp
     n12 = n12[len(n12)//2-maxLags: len(n12)//2+maxLags+1]
     n23 = n23[len(n23)//2-maxLags: len(n23)//2+maxLags+1]
     n13 = n13[len(n13)//2-maxLags: len(n13)//2+maxLags+1]
 
     # Finds index of maximum value and translates it into lag in samples
-    #print(str(np.argmax(n12)) + " " + str(np.argmax(n23)) + " " + str(np.argmax(n13))) Things are currently working and as such this only causes
+    print(str(np.argmax(n12)-maxLags) + " " + str(np.argmax(n23)-maxLags) + " " + str(np.argmax(n13)-maxLags))
     l12 = -(np.argmax(n12) - maxLags)
     l23 = -(np.argmax(n23) - maxLags)
     l13 = -(np.argmax(n13) - maxLags)
@@ -64,41 +67,63 @@ def interpolate(sample_period, data, factor):
     return new_data, new_sample_period
 
 
+def importAndCalculate(plotCorrs = 0):
+    #Channel  2 is "upper left", channel 3 is "lower mid", channel 4 is "upper right"
+    # Import data from bin file
+    sample_period, data_raw = raspi_import('adcData.bin')
+    data = data_raw[5:,:]
 
-#Channel  2 is "upper left", channel 3 is "lower mid", channel 4 is "upper right"
-# Import data from bin file
-sample_period, data_raw = raspi_import('adcData.bin')
-data = data_raw[:,:]
+    data = signal.detrend(data, axis=0)  # removes DC component for each channel
+    sample_period *= 1e-6  # change unit to micro seconds
 
-data = signal.detrend(data, axis=0)  # removes DC component for each channel
-sample_period *= 1e-6  # change unit to micro seconds
+    #Interpolate
+    interpolation_factor = 3
+    #data, sample_period = interpolate(sample_period, data, interpolation_factor) #Caused major issues so commented out for now
 
-#Interpolate
-interpolation_factor = 1
-data, sample_period = interpolate(sample_period, data, interpolation_factor)
+    #make crosscorelations
+    n12, n23, n13 = make_correlations(data, 2, 3, 4)
 
-#make crosscorelations
-n12, n23, n13 = make_correlations(data, 2, 3, 4)
+    #Find number of lags
+    l12, l23, l13 = find_lags(n12, n23, n13, interpolation_factor)
 
-#Find number of lags
-l12, l23, l13 = find_lags(n12, n23, n13)
+    #print(f"Lags: 1-2:{l12}, 2-3:{l23}, 1-3:{l13}") Usefull for testing, useless right now
 
-#print(f"Lags: 1-2:{l12}, 2-3:{l23}, 1-3:{l13}") Usefull for testing, useless right now
+    #Calculate the angle
+    theta = get_angle(l12, l23, l13)
 
-#Calculate the angle
-theta = get_angle(l12, l23, l13)
+    print(f"Angle: {theta}")
 
-print(f"Angle: {theta}")
+    if(plotCorrs):
+        plot_correlations(n12, n23, n13)
 
+    return(theta)
 
-# Plots:
-plt.subplot(3, 1, 1)
-plt.plot(np.linspace(-len(n12)//2-1,  len(n12)//2, len(n12)), n12, ".")
-plt.subplot(3, 1, 2)
-plt.plot(np.linspace(-len(n12)//2-1, len(n12)//2, len(n12)), n23, ".")
-plt.subplot(3, 1, 3)
-plt.plot(np.linspace(-len(n12)//2-1, len(n12)//2, len(n12)), n13, ".")
+def plot_correlations(n12, n23, n13):
+    #Plots: commented out for quick testing
+    plt.subplot(3, 1, 1)
+    plt.plot(np.linspace(-len(n12)//2-1,  len(n12)//2, len(n12)), n12, ".")
+    plt.subplot(3, 1, 2)
+    plt.plot(np.linspace(-len(n12)//2-1, len(n12)//2, len(n12)), n23, ".")
+    plt.subplot(3, 1, 3)
+    plt.plot(np.linspace(-len(n12)//2-1, len(n12)//2, len(n12)), n13, ".")
+    plt.show()
 
+    return
 
+def sampleAndStart(): #The function that calls for the RPi to begin sampling, transfer to local and then calls function importAndCalculate
+    N_runs = int(input("How many times do you want to run the test?"))
+    waitTime = 30
+    angles = np.zeros(N_runs)
 
-plt.show()
+    for i in range(N_runs):
+        sleep(waitTime)
+        call(r'angleFinder.bat')
+        angles[i] = importAndCalculate() #Starts importAndCalculate and stores the found angle for later use
+        print(f"Test {i} finished, waiting {waitTime} seconds.")
+
+    for ang in angles:
+        print(ang, sep = " , ", end=" ")
+
+    return()
+
+sampleAndStart()
--- angleFinder.bat
+++ angleFinder.bat
@@ -1,4 +1,3 @@
 ::This script does not update you on where in the process it currently is, nice for my use but may be trash in other settings
 @ssh pi@hostname.local "sudo /home/pi/adc_sampler/adc_sampler 31250"
 @scp pi@hostname.local:/home/pi/adcData.bin adcData.bin
-@py .\acoustics.py
