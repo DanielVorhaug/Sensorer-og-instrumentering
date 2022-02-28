6cbc7cdf5b79acdb96684e66e0a0b24e7693341c
Carl Thyness <carl.thyness@gmail.com>
2022-02-28 12:00:47 +0100

    Fixed arctan in getAngle and improved UX

--- acoustics.py
+++ acoustics.py
@@ -20,10 +20,12 @@ def make_correlations(data, wanted_axis1, wanted_axis2, wanted_axis3):
 
 
 def get_angle(l12, l23, l13):
-    angl = np.arctan(np.sqrt(3))*(-l12 - l13)/(-l12 +l13 + 2*l23)
-    #angl -= 1.05 #To control what is 0 radians
+    angl = np.arctan(np.sqrt(3)*(-l12 - l13)/(-l12 +l13 + 2*l23))
+    angl -= np.pi/2 #To have the result be between -pi and pi
     if(l12 - l13 - 2*l23 < 0):
-        angl = angl + np.pi
+        angl += np.pi
+    angl = (angl/np.pi) * 180 #Translates the angle into degrees
+    angl = -angl
     
     return(angl)
 
@@ -34,7 +36,7 @@ def find_lags(n12, n23, n13, N_interp):
     n13 = n13[len(n13)//2-maxLags: len(n13)//2+maxLags+1]
 
     # Finds index of maximum value and translates it into lag in samples
-    print(str(np.argmax(n12)-maxLags) + " " + str(np.argmax(n23)-maxLags) + " " + str(np.argmax(n13)-maxLags))
+    print(str(-np.argmax(n12)+maxLags) + " " + str(-np.argmax(n23)+maxLags) + " " + str(-np.argmax(n13)+maxLags))
     l12 = -(np.argmax(n12) - maxLags)
     l23 = -(np.argmax(n23) - maxLags)
     l13 = -(np.argmax(n13) - maxLags)
@@ -71,14 +73,19 @@ def importAndCalculate(plotCorrs = 0):
     #Channel  2 is "upper left", channel 3 is "lower mid", channel 4 is "upper right"
     # Import data from bin file
     sample_period, data_raw = raspi_import('adcData.bin')
-    data = data_raw[5:,:]
+    data = data_raw[5000:,:]
+
+
+    #If you need to see the raw data
+    # plt.plot(np.linspace(-len(data_raw)//2 + 1, len(data_raw)//2, len(data_raw)), data_raw[:, 2], ".")
+    # plt.show()
 
     data = signal.detrend(data, axis=0)  # removes DC component for each channel
     sample_period *= 1e-6  # change unit to micro seconds
 
-    #Interpolate
-    interpolation_factor = 1 #This should be set to one as long as we do not interpolate
-    #data, sample_period = interpolate(sample_period, data, interpolation_factor) #Caused major issues so commented out for now
+    #Interpolation
+    interpolation_factor = 3 #This should be set to one as long as we do not interpolate
+    data, sample_period = interpolate(sample_period, data, interpolation_factor) #Caused major issues so commented out for now
 
     #make crosscorelations
     n12, n23, n13 = make_correlations(data, 2, 3, 4)
@@ -86,7 +93,7 @@ def importAndCalculate(plotCorrs = 0):
     #Find number of lags
     l12, l23, l13 = find_lags(n12, n23, n13, interpolation_factor)
 
-    #print(f"Lags: 1-2:{l12}, 2-3:{l23}, 1-3:{l13}") Usefull for testing, useless right now
+    #print(f"Lags: 1-2: {l12}, 2-3: {l23}, 1-3: {l13}") #Usefull for testing
 
     #Calculate the angle
     theta = get_angle(l12, l23, l13)
@@ -99,7 +106,12 @@ def importAndCalculate(plotCorrs = 0):
     return(theta)
 
 def plot_correlations(n12, n23, n13):
-    #Plots: commented out for quick testing
+    #To make it only plot the center value +-5
+    # n12 = n12[len(n12)//2 - 5: len(n12)//2 + 6]
+    # n23 = n23[len(n23)//2 - 5: len(n23)//2 + 6]
+    # n13 = n13[len(n13)//2 - 5: len(n13)//2 + 6]
+
+
     plt.subplot(3, 1, 1)
     plt.plot(np.linspace(-len(n12)//2 + 1, len(n12)//2, len(n12)), n12, ".")
     plt.subplot(3, 1, 2)
@@ -110,24 +122,30 @@ def plot_correlations(n12, n23, n13):
 
     return
 
-def sampleAndStart(): #The function that calls for the RPi to begin sampling, transfer to local and then calls function importAndCalculate
-    N_runs = int(input("How many times do you want to run the test?"))
-    waitTime = 30
+def sampleAndStart(once = 0, plot = 0): #The function that calls for the RPi to begin sampling, transfer to local and then calls function importAndCalculate
+    if(once):
+        N_runs = 1
+    else:
+        N_runs = int(input("How many times do you want to run the test?"))
+
+    waitTime = 5
     angles = np.zeros(N_runs)
 
     for i in range(N_runs):
         sleep(waitTime)
         call(r'angleFinder.bat')
-        angles[i] = importAndCalculate() #Starts importAndCalculate and stores the found angle for later use
+        angles[i] = importAndCalculate(plot) #Starts importAndCalculate and stores the found angle for later use
         print(f"Test {i} finished, waiting {waitTime} seconds.")
 
     print() #Just to get a line of seperation
-    # var = np.var(angles)
-    # print(f"Variance was estimated to {var} radians. Angles will come on next line")
+    if(N_runs > 1):
+        var = np.var(angles)
+        print(f"Variance was estimated to {var} degrees. Angles will come on next line")
+
     for ang in angles:
         print(ang, sep = ", ", end=" ")
 
     return
 
-#sampleAndStart()
-importAndCalculate(1)
+sampleAndStart()
+#importAndCalculate(1)
