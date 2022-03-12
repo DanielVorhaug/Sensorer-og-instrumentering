54c850b4f7aeaca16297f96db65b49eaef830bcc
Carl Thyness <carl.thyness@gmail.com>
2022-03-12 11:47:57 +0100

    Fixed issues, improved function separation. In short: improved

--- radar.py
+++ radar.py
@@ -1,3 +1,4 @@
+from cmath import log10
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
@@ -20,24 +21,62 @@ def raspi_import(path, channels=5):
         data = data.reshape((-1, channels))
     return sample_period, data
 
-def plot_FFTs(to_plot, f):
+def plot_raw(to_plot, Ts,  I_channel = 0):
+    t=np.linspace(0, Ts*to_plot.shape[0], to_plot.shape[0]) #There is a better function than linspace for this, but I do not remeber the name
+
     number_of_plots = to_plot.shape[1]
     for i in range(number_of_plots):
         plt.subplot(number_of_plots, 1, i+1)
+        if(i == I_channel):
+            plt.title("I-channel")
+        else:
+            plt.title("Q-channel")
         plt.xlabel("Frequency [Hz]")
         plt.ylabel("Power [dB]")
-        plt.plot(f, to_plot[:, i], ".")
+        plt.plot(t, to_plot[:, i], ".")
         plt.grid()
     
     plt.show()
 
     return
 
-def calc_speed(fd):
+def plot_FFTs(to_plot, Ts, I_channel = 0):
+    FFT = sp.fft.fft(to_plot, axis=0) #Calculates the FFT of each channel
+    freqs = sp.fft.fftfreq(to_plot.shape[0], Ts)
 
+    #shifting frequencies for nicer plots
+    FFT = sp.fft.fftshift(FFT)
+    freqs = sp.fft.fftshift(freqs)
+    spectrum = 10*np.log10(FFT)
+
+    number_of_plots = FFT.shape[1]
+    for i in range(number_of_plots):
+        plt.subplot(number_of_plots, 1, i+1)
+        if(i == I_channel):
+            plt.title("I-channel")
+        else:
+            plt.title("Q-channel")
+        plt.xlabel("Frequency [Hz]")
+        plt.ylabel("Power [dB]")
+        plt.plot(freqs, spectrum[:, i], ".")
+        plt.grid()
+    
+    plt.show()
+
+    return
+
+def calc_speed(sig, Ts,  I_channel = 0, Q_channel = 1):
     #Removed a factor of 10^8 from f0 and c as they get divided by each other
     f0 = 24.13*10 #Frequency of the radar
     c = 3 #Speed of light in free space (close enough for our use)
+
+    complex_data = sig[:, I_channel] + sig[:, Q_channel]*1j
+    FFT = sp.fft.fft(complex_data)
+    freqs = sp.fft.fftfreq(complex_data.shape[0], Ts)
+
+    #Finds the frequency caused by movement
+    peak = np.argmax(FFT) #The channel does not matter, but we must choose one
+    fd = freqs[peak] #Doppler frequency (hopefully)
 
     v = (c*fd)/(2*f0)
 
@@ -51,28 +90,21 @@ def find_speed(path, I_channel = 0, Q_channel = 1):
     # plt.show()
 
     #Just some spring cleaning
-    data = data_raw[:, 0:2] #removes data from ADCs not in use
+    data = data_raw[:, 0:2] #removes data from ADCs not in use (Requires that I and Q use ADC 0 and 1)
     data = signal.detrend(data, axis=0)  #removes DC component for each channel (should not matter as we have a filter)
     sample_period *= 1e-6  # change unit to micro seconds
 
-    FFT = sp.fft.fft(data, axis=0) #Calculates the FFT of each channel
-    FFT = sp.fft.fftshift(FFT)
-    freqs = sp.fft.fftfreq(n = data.shape[0], d=sample_period)
-    freqs = sp.fft.fftshift(freqs)
-
-    plot_FFTs(FFT, freqs)
-
-    #Finds the frequency caused by movement
-    peak = np.argmax(np.abs(FFT[:, I_channel])) #The channel does not matter, but we must choose one
-    peak_freq = freqs[peak]
+    plot_FFTs(data, sample_period, I_channel)
 
     #Calculates the speed of movement from the frequency
-    speed = calc_speed(peak_freq)
+    speed = calc_speed(data, sample_period, I_channel, Q_channel)
 
+    print(speed)
     return(speed)
 
     
 
 
 filepath = 'adcData.bin' #Filepath to datafile
-find_speed(filepath)
+v = find_speed(filepath)
+print(v)
