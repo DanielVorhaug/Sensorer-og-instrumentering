ba496b9ac36384c575b41e7033684c06c8a7e1db
Daniel Vorhaug <dvorhaug@gmail.com>
2022-03-14 16:06:07 +0100

    change script

--- script.bat
+++ script.bat
@@ -1,3 +1,3 @@
-ssh -x pi@hostname.local "sudo /home/pi/adc_sampler/adc_sampler 10000"
+ssh -x pi@hostname.local "sudo /home/pi/adc_sampler/adc_sampler 31500"
 scp pi@hostname.local:/home/pi/adcData.bin adcData.bin
-py .\raspi_analyze.py
+py .\radar.py
